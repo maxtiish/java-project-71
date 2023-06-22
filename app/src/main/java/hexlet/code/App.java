8219231537166af9fba8f179ed02194d2b8c8375
maxtiish <cearness@mail.ru>
2023-06-22 12:37:05 +0700

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,23 +4,31 @@ import picocli.CommandLine;
 
 @CommandLine.Command(name="gendiff",
         description="Compares two configuration files and shows a difference.")
+
 class App implements Runnable {
 
+    @CommandLine.Option(names = {"-f", "--format"}, defaultValue = "stylish",
+            description = " output format [default: ${DEFAULT-VALUE}]",  paramLabel = "format")
+    private String format;
+
     @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true,
-            description = "Show this help message and exit.")
+            description = "Show this @|fg(blue) help|@ message and exit.")
     boolean usageHelpRequested;
 
     @CommandLine.Option(names = {"-V", "--version"}, versionHelp = true,
             description = "Print version information and exit.")
     boolean versionInfoRequested;
 
+    @CommandLine.Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    String filepath1;
+
+    @CommandLine.Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    String filepath2;
+
     public void run() {
     }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
-
         CommandLine commandLine = new CommandLine(new App());
         commandLine.parseArgs(args);
         if (commandLine.isUsageHelpRequested()) {
@@ -30,5 +38,8 @@ class App implements Runnable {
             commandLine.printVersionHelp(System.out);
             return;
         }
+
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
